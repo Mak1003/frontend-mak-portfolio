490a790bb7f0f0c3e300acdd329da7b9f17338b0
MAK <maheshkharote2580@gmail.com>
2024-12-18 01:00:54 +0530

    update the Academics

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Education from './components/Education';
-import Academics from './components/Academics';  
+import Academics from './components/Sitacademics';  
 
 function App() {
   return (
